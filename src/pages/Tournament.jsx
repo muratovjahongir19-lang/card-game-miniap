d447459e25370a35742e8c7fdea10fd824099e02
muratovjahongir19-lang <muratovjahongir19@gmail.com>
2026-06-17 01:08:59 +0500

    Tournament.jsx

---
+++ src/pages/Tournament.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+
+export default function Tournament(){
+
+
+
+return(
+
+<div className="card">
+
+
+<h1>
+🏆 Турниры
+</h1>
+
+
+
+<div className="tournament">
+
+
+<h2>
+Большой турнир
+</h2>
+
+
+<p>
+Участники: 256 игроков
+</p>
+
+
+<p>
+Приз: 1 000 000 🪙
+</p>
+
+
+
+<button>
+
+Войти
+
+</button>
+
+
+</div>
+
+
+
+</div>
+
+)
+
+}
