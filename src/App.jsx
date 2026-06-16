26f5de54f115b2a2a659c65ae0dbbc8ce98d0894
muratovjahongir19-lang <muratovjahongir19@gmail.com>
2026-06-17 00:10:05 +0500

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,166 @@
+import {useState} from "react";
+
+
+function App(){
+
+
+const [page,setPage]=useState("home");
+
+
+return(
+
+<div className="app">
+
+
+<header>
+
+<h1>
+♠ CARD GAME
+</h1>
+
+<div>
+🪙 25 000
+💎 350
+</div>
+
+</header>
+
+
+
+{
+
+page==="home" &&
+
+<main>
+
+
+<div className="profile">
+
+<img src="https://i.pravatar.cc/100"/>
+
+<h2>
+Игорь_user
+</h2>
+
+<p>
+LVL 23
+</p>
+
+</div>
+
+
+
+<div className="season">
+
+<h2>
+Сезон 1
+</h2>
+
+<p>
+Путь чемпиона 🏆
+</p>
+
+</div>
+
+
+
+
+<button className="play">
+
+БЫСТРАЯ ИГРА
+
+</button>
+
+
+
+<div className="buttons">
+
+<button>
+1 VS 1
+</button>
+
+<button>
+4 игрока
+</button>
+
+<button>
+6 игроков
+</button>
+
+
+</div>
+
+
+</main>
+
+
+}
+
+
+
+{
+
+page==="profile" &&
+
+<div className="card">
+
+<h2>
+Профиль
+</h2>
+
+<p>
+Игрок: Игорь_user
+</p>
+
+<p>
+ELO: 1250
+</p>
+
+<p>
+Winrate: 68%
+</p>
+
+</div>
+
+
+}
+
+
+
+
+
+<nav>
+
+
+<button onClick={()=>setPage("home")}>
+🏠
+</button>
+
+
+<button onClick={()=>setPage("profile")}>
+👤
+</button>
+
+
+<button>
+💬
+</button>
+
+
+<button>
+⚙️
+</button>
+
+
+</nav>
+
+
+
+</div>
+
+)
+
+}
+
+
+export default App;
