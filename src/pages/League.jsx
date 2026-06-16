a4d7a343ffe416e14645aa00d59da3541a73d79c
muratovjahongir19-lang <muratovjahongir19@gmail.com>
2026-06-17 01:09:35 +0500

    League.jsx

---
+++ src/pages/League.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+
+export default function League(){
+
+
+
+return(
+
+<div className="card">
+
+
+<h1>
+🏅 Лига
+</h1>
+
+
+
+<h2>
+🥉 Бронза
+</h2>
+
+
+<p>
+Рейтинг ELO: 1000
+</p>
+
+
+<p>
+До серебра: 200 очков
+</p>
+
+
+
+</div>
+
+
+)
+
+}
