5457b5b044344b876ba571425b88a8876de286c2
muratovjahongir19-lang <muratovjahongir19@gmail.com>
2026-06-17 12:49:46 +0500

    shuffle.js

---
+++ backend/game/shuffle.js
@@ -0,0 +1,23 @@
+function shuffle(deck){
+
+for(
+let i=deck.length-1;
+i>0;
+i--
+){
+
+const j=
+Math.floor(
+Math.random()*(i+1)
+);
+
+[deck[i],deck[j]]=
+[deck[j],deck[i]];
+
+}
+
+return deck;
+
+}
+
+module.exports = shuffle;
